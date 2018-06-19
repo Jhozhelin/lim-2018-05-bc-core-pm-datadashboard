5be176607612526f8e1113037adc65c56fbcad63
Jhozhelin <jhozhelinzuniga@gmail.com>
2018-06-19 18:03:33 -0500

    modificando html

--- src/data.js
+++ src/data.js
@@ -1,29 +1,24 @@
-fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+/*En este espacio van 4 funciones*/
+// const XHR = new XMLHttpRequest () ;
+// console.log (XHR) ;
+
+// XHR.onreadystatechange = function(){
+//     if (XHR.readyState == 4 && XHR. status == 200){
+//         console.log(JSON.parse(XHR.responseText)) ;
+
+//         console.log(XHR.res)
+//        }else{
+//            console.log('Hay un problema en el servidor') ;
+//     }
+// }
+// XHR.open('Get','http://http://127.0.0.1:5500/data/cohot.json') ;
+// XHR.send() ;
+fetch('/data/cohorts.json')
 // fetch('http://http://127.0.0.1:5500/data/cohorts.json')
-/* .then(function(response){
+.then(function(response){
     return response.json() ;
 })
-.then(function(myJson){
-    console.log(myJson)
+.then(function(data){
+    console.log(typeof data);
+    console.log(data[0].id)
 });
- */
-.then(response => response.json())
-.then(json => {
-    const idAndNames = json;
-
-    console.log(idAndNames);
-     for(let i=0; i < idAndNames.length; i++){
-         const pruebaRecepcion = document.getElementById("pruebaRecepcion");
-         const nameList = document.getElementById("nameList");
-/*         const names = document.createTextNode(idAndNames[i].id); */ 
-        const show= idAndNames[i].name;
-        
-        pruebaRecepcion.addEventListener("click", () => {
-            nameList.innerHTML += show;
-        } )
-
-    }
-    
-});
-
-
