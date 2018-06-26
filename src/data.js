c1dfc61a3fab9e28a72093801afd63c603561a2d
alexandra.vejarano@gmail.com <alexandra.vejarano@gmail.com>
2018-06-25 22:07:52 -0500

    Agregando indicaciones de cómo trabajar con fetch en data.js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,31 @@
-const dataCohorts = fetch('../data/cohorts.json'
+//***APRENDIENDO A USAR PROCESOS ASÍNCRONOS */
+let cohortsRaw = []
+let progressRaw = {}
+let usersRaw = []
+
+//Paso uno, traemos la data de los json con fetch y los guardamos en variables
+const dataCohorts = fetch('../data/cohorts.json')
 .then(response => response.json())
-.then( data => {x(data)})
+.then( data => { //Era necesario tener un segundo then para ejecutar otro función, lugar de 'data', puede ir cualquier otro nombre
+    cohortsRaw = data //Arriba creamos un arreglo para guardar nuestro resultado y lo igualamos a data
+    console.log(cohortsRaw) //Probamos en consola
+} )
 
 const dataProgress = fetch ('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
 .then(response => response.json())
+.then(data => {
+    progressRaw = data
+    console.log(progressRaw)
+})
 
 const dataUsers = fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
 .then(response => response.json())
-
-console.log(dataCohorts[1])
-
-console.log(dataUsers[1])
-
-const exampleArray = ['a', 'b', 'c', 'd']
-console.log(exampleArray[1])
+.then(data => {
+    usersRaw = data
+    console.log(usersRaw)
+}) 
+//Al final, nos daremos cuenta que todos los fetch no se ejecutan de arriba hacia abajo 
+/* Promise.all([dataCohorts]).then(array => {
+    console.log(array[0])
+})
+ */
