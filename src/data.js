eb85e2fee738df34352d05a70a6d04507aa46033
alexandra.vejarano@gmail.com <alexandra.vejarano@gmail.com>
2018-06-27 14:11:25 -0500

    agregando estructura de funciones

--- src/data.js
+++ src/data.js
@@ -1,20 +1,62 @@
 //Primera función
 const computeUsersStats = (users, progress, courses) => {
-    console.log (users)
+    let userWithStats = []
+    let userStats = {}
+    
+    userStats.stats = {
+        percent: a,
+        excercises: {
+            total: b,
+            completed: c,
+            percent: d
+        },
+        reads: {
+            total: e,
+            completed: f,
+            percent: g
+        },
+        quizzes: {
+            total: h,
+            completed: i,
+            percent: j,
+            scoreSum: k,
+            scoreAvg: l
+        }
+        }
+        
+    userWithStats.push(userStats)
+        
+    
+    
+    return usersWithStats 
+    
+
 }
+
+    
 
 
 //Segunda función
 const sortUsers =  (users, orderBy, orderDirection) => {
-
+    //users: Arreglo de objetos creado con computeUsersStats().
+//orderBy: String que indica el criterio de ordenado. Debe permitir ordenar por nombre, porcentaje de completitud total, porcentaje de ejercicios autocorregidos completados, porcentaje de quizzes completados, puntuación promedio en quizzes completados, y porcentaje de lecturas completadas.
+//orderDirection: La dirección en la que queremos ordenar. Posibles valores: ASC y DESC (ascendiente y descendiente).
+    return //Arreglo de usuarios ordenado.
 }
 
 //Tercerca función
 const filterUsers = (users, search) => {
+    //users: Arreglo de objetos creado con computeUsersStats()
+    //search: String de búsqueda
 
+    return //Nuevo arreglo de usuarios incluyendo solo aquellos que cumplan la condición de filtrado, es decir, aquellos que contengan el string search en el nombre (name) del usuario.
 }
 
 //Cuarta función
 const processCohortData = (options) => {
-
+    let computedData = computeUsersStats (options.chortData.users, options.cohortData.progress) //courses
+    let sortedData = sortUsers (computedData) //orderBy, orderDirection
+    let filteredData = filterUsers (sortedData) //search
+    
+    return filteredData//Nuevo arreglo de usuarios ordenado y filtrado con la propiedad stats añadida
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 let cohortsDataRaw = []
 let progressDataRaw = {}
 let usersDataRaw = []
+let options = {}
 
 const dataCohorts = fetch('../data/cohorts.json')
 .then(response => response.json())
@@ -21,6 +22,16 @@ Promise.all([dataCohorts, dataProgress, dataUsers]).then (data =>{
     usersDataRaw = data [2]
 
 })
-
+//***CREANDO ARGUMENTO OPTIONS */
+options = {
+    cohort: cohortsDataRaw[31], //Objeto cohort (de la lista de cohorts)
+    cohortData: {
+        users: usersDataRaw, //Arreglo de usuarios miembros del cohort
+        progress: progressDataRaw, //Objeto con data de progreso de cada usuario en el contexto de un cohort en particular
+    },
+    orderBy: "d", //String con criterio de ordenado (ver sortUsers).
+    orderDirections: "e", //String con dirección de ordenado (ver sortUsers)
+    Search: "g" //String de búsqueda (ver filterUsers)
+}
 //***AGREGANDO EVENTOS */ 
 
